16fc8fbb5987a45cd0bafe7ed33cfb9f6ada7cd7
Gilcemir <gangelofilho@gmail.com>
2025-11-22 08:14:37 -0300

    Ajusta espaçamento Adiciona mml: às tags

--- src/MathFlow/Infrastructure/Converters/OmmlToMathMLConverter.cs
+++ src/MathFlow/Infrastructure/Converters/OmmlToMathMLConverter.cs
@@ -1,8 +1,9 @@
+using System.Text.RegularExpressions;
 using Jering.Javascript.NodeJS;
 
 namespace MathFlow.Infrastructure.Converters;
 
-public class OmmlToMathMLConverter
+public partial class OmmlToMathMLConverter
 {
     private readonly INodeJSService _nodeJsService;
     private const string SCRIPT_PATH = "converter.js";
@@ -26,11 +27,30 @@ public class OmmlToMathMLConverter
                 args: new[] { omml }
             );
 
-            return result ?? string.Empty;
+            return ManipulateHtml(result);
         }
         catch (Exception ex)
         {
             throw new InvalidOperationException($"Failed to convert OMML to MathML: {ex.Message}", ex);
         }
     }
+    private static string ManipulateHtml(string? html)
+    {
+        if(string.IsNullOrEmpty(html))
+            return string.Empty;
+        // Substituir todas as ocorrências de <tagName> por <mml:tagName>
+        html = MyRegex().Replace(html, "<mml:$1");
+
+        // Substituir todas as ocorrências de </tagName> por </mml:tagName>
+        html = MyRegex1().Replace(html, "</mml:$1>");
+        html = MyRegex2().Replace(html, "");
+        return html;
+    }
+
+    [GeneratedRegex(@"<(\w+)")]
+    private static partial Regex MyRegex();
+    [GeneratedRegex(@"<\/(\w+)>")]
+    private static partial Regex MyRegex1();
+    [GeneratedRegex(@"\s\s+")]
+    private static partial Regex MyRegex2();
 }
